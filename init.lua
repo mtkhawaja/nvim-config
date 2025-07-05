0e9964d0b0d9fdc198562858c6bd6891a25fc117
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2025-07-05 02:13:28 -0400

    feat: Changed theme from one-dark to catppuccin.

--- init.lua
+++ init.lua
@@ -190,14 +190,14 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
+   {
+     'catppuccin/nvim',
+     name = 'catppuccin',
+     priority = 1000,
+     config = function()
+        vim.cmd.colorscheme 'catppuccin-mocha'
+     end,
+   },
 
   {
     -- Set lualine as statusline
